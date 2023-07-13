07f5a00fd5722ae6cbfe04e5b83b4e479106d140
Minh-Phuc Nguyen <mphncs@gmail.com>
2023-07-13 10:01:30 +0700

    implement smooth navigation

--- js/script.js
+++ js/script.js
@@ -6,6 +6,15 @@ const btnFormSubmit = document.querySelector('.modal__btn');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const buttonsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const featuresSect = document.querySelector('#features');
+
+const allNavLinks = [
+  document.querySelector('.nav'),
+  document.querySelector('.footer__nav'),
+];
+
+// Modal window
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -26,3 +35,30 @@ document.addEventListener('keydown', e => {
 
 overlay.addEventListener('click', closeModal);
 btnCloseModal.addEventListener('click', closeModal);
+
+// Button scroll to
+btnScrollTo.addEventListener('click', () =>
+  featuresSect.scrollIntoView({ behavior: 'smooth' })
+);
+
+// Smooth scrolling
+allNavLinks.forEach(el =>
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    if (
+      e.target.classList.contains('nav__logo') ||
+      e.target.classList.contains('nav__link') ||
+      e.target.classList.contains('footer__link')
+    ) {
+      const idSection = e.target.getAttribute('href');
+
+      if (idSection && idSection !== '#')
+        document
+          .querySelector(idSection)
+          .scrollIntoView({ behavior: 'smooth' });
+      else if (!e.target.classList.contains('btn--show-modal'))
+        document.body.scrollIntoView({ behavior: 'smooth' });
+    }
+  })
+);
